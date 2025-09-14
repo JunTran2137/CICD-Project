fbf7b634eb86b9c10905ef3e657313941546b1ec
dungtt112 <tridung2137@gmail.com>
2025-09-14 11:49:36 +0700

    test cicd

--- backend/Data/ZtblSkipLabel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Backend.Data;
-
-public partial class ZtblSkipLabel
-{
-    public int LabelCount { get; set; }
-}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import BowlersTable from './BowlersTable';
+import AppProvider from "@/providers/AppProvider";
+
+import { AppRoutes } from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <Header
-        title="BLE Bowlers"
-        description="One of the most competitive and well-regarded bowling leagues this side of the 
-          world, here are some of the best bowlers we have. The bowlers displayed here are either on team 
-          Marlins or team Sharks."
-      />
-      <br />
-      <BowlersTable displayTeams={['Marlins', 'Sharks']} />
-    </div>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
   );
 }
 
